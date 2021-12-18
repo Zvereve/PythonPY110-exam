c976fb06a682ea69c755f1886003314a0588008a
Evgeniy <dr.jekkill@yandex.ru>
2021-12-18 19:45:03 +0300

    решено без дополнительного задания

--- main.py
+++ main.py
@@ -1,60 +1,70 @@
 import random
+import json
 from conf import MODEL
 from faker import Faker
 
 
-def pk(start=1):
+def pk(value = 1):
+    """Функция счетчик"""
     while True:
-        yield start
-        start += 1
+        yield value
+        value += 1
 
 
 def tit():
+    """Функция возврта из файла рандомного наименования книги"""
     with open('books.txt', 'r') as f:
-        x = random.randint(1, len(f.readlines()))
+        x = random.randint(0, len(f.readlines())-1)
 
-    return x
+    with open('books.txt', 'r', encoding="utf-8") as f:
+        y = list(f.readlines())
+    return y[x].replace("\n", "")
 
 
 def ff():
+    """Функция возвращающая рандомный международный стандартный код книги"""
     fake = Faker()
     return fake.bothify(text="%%%-%-%%%%%-%%%-%")
 
 
 def autors():
-    x = random.randint(1, 3)
-    st =[]
+    """Функция возвращает авторов книг от 1 до 3х"""
+    st = []
     fake = Faker()
-    for i in range(x):
+    for i in range(random.randint(1, 3)):
         st.append(fake.name())
     return st
 
 
 def booklist():
+    """Функция возвращает словарь"""
     yield {
-            "model": MODEL,
-            "pk": next(pk()),
-            "fields": {
-                       "title": tit(),
-                       "year": random.randint(1900, 2021),
-                       "pages": random.randint(100, 2000),
-                       "isbn13": ff(),
-                       "rating": round(random.triangular(0, 5), 2),
-                       "price": round(random.triangular(100, 10000), 2),
-                       "author": [
-                                  autors()
-                                 ]
-                       }
-            }
+               "model": MODEL,
+               "pk": next(coun),
+               "fields": {
+                          "title": tit(),
+                          "year": random.randint(1900, 2021),
+                          "pages": random.randint(100, 2000),
+                          "isbn13": ff(),
+                          "rating": round(random.triangular(0, 5), 2),
+                          "price": round(random.triangular(100, 10000), 2),
+                          "author": [
+                                     autors()
+                                    ]
+                          }
+               }
 
 
 def main():
-    #stt = {}
-    for x in range(100):
-        print(next(booklist()))
-
-
+    """Функция генератор списка книг и записи списка в фаил"""
+    st = []
+    for _ in range(100):
+        st.append(next(booklist()))
+        print(st)
+    with open('UserList.json', 'w', encoding="utf-8") as f:
+        json.dump(st, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
+    coun = pk()
     main()
